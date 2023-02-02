b2cf2b3be223642c6031014e8b508276ff7af819
xenonv1 <82435371+xenonv1@users.noreply.github.com>
2023-02-02 10:03:28 +0100

    Initial Commit

---
+++ src/components/InteractionBarComponent.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="main-container">
+    <div class="insert-container">
+      <input type="text" placeholder="Insert your item..." />
+      <Button><i class="fa-solid fa-plus"></i></Button>
+    </div>
+    <Button class="options-button"><i class="fa-solid fa-bars"></i></Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InteractionBarComponent",
+};
+</script>
+
+<style scoped>
+.main-container {
+  /* general positioning */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  margin: auto;
+
+  /* styling */
+  background-color: cornflowerblue;
+  padding: 1rem;
+  border-radius: 15px;
+}
+
+.input-container {
+  display: flex;
+}
+
+input {
+  background-color: lightblue;
+  border: 0px;
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
+  padding: 0.2rem 1rem 0.2rem 1rem;
+  width: 50vw;
+}
+
+input:focus {
+  outline: none;
+}
+
+input::placeholder {
+  color: black;
+  font-style: italic;
+}
+
+.insert-container button {
+  background-color: lightblue;
+  border: none;
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+  padding: 0.2rem;
+}
+
+.options-button {
+  background-color: lightblue;
+  border: none;
+  border-radius: 5px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <InteractionBar />
+</template>
+
+<script>
+import InteractionBar from "../components/InteractionBarComponent.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    InteractionBar,
+  },
+};
+</script>
